56836e901458bd4c1afef7dab77c49e93b963edc
sandy <phalodi@gmail.com>
2017-01-18 01:29:50 +0530

    support carbon shell with spark 2.1

--- integration/spark2/src/main/scala/org/apache/spark/repl/CarbonSparkILoop.scala
+++ integration/spark2/src/main/scala/org/apache/spark/repl/CarbonSparkILoop.scala
@@ -17,56 +17,55 @@
 
 package org.apache.spark.repl
 
-class CarbonSparkILoop extends SparkILoop {
+class CarbonSpark2ILoop extends SparkILoop {
 
   override def initializeSpark() {
     intp.beQuietDuring {
       processLine("""
-         if(org.apache.spark.repl.carbon.Main.interp == null) {
-           org.apache.spark.repl.carbon.Main.main(Array[String]())
-         }
-              """)
-      processLine("val i1 = org.apache.spark.repl.carbon.Main.interp")
-      processLine("import i1._")
-      processLine("""
-         @transient val sc = {
-           val _sc = i1.createSparkContext()
-           println("Spark context available as sc.")
-           _sc
-         }
-              """)
+        @transient val spark = if (org.apache.spark.repl.Main.sparkSession != null) {
+            org.apache.spark.repl.Main.sparkSession
+          } else {
+            org.apache.spark.repl.Main.createSparkSession()
+          }
+        @transient val sc = {
+          val _sc = spark.sparkContext
+          if (_sc.getConf.getBoolean("spark.ui.reverseProxy", false)) {
+            val proxyUrl = _sc.getConf.get("spark.ui.reverseProxyUrl", null)
+            if (proxyUrl != null) {
+              println(s"Spark Context Web UI is available at ${proxyUrl}/proxy/${_sc.applicationId}")
+            } else {
+              println(s"Spark Context Web UI is available at Spark Master Public URL")
+            }
+          } else {
+            _sc.uiWebUrl.foreach {
+              webUrl => println(s"Spark context Web UI available at ${webUrl}")
+            }
+          }
+          println("Spark context available as 'sc' " +
+            s"(master = ${_sc.master}, app id = ${_sc.applicationId}).")
+          println("Spark session available as 'spark'.")
+          _sc
+        }
+                  """)
+      processLine("import java.io.File")
+      processLine("import org.apache.commons.io.FileUtils")
       processLine("import org.apache.spark.SparkContext._")
-      processLine("import org.apache.spark.sql.CarbonContext")
-      processLine("""
-         @transient val cc = {
-           val _cc = {
-             import java.io.File
-             val path = System.getenv("CARBON_HOME") + "/bin/carbonshellstore"
-             val store = new File(path)
-             store.mkdirs()
-             val storePath = sc.getConf.getOption("spark.carbon.storepath")
-                  .getOrElse(store.getCanonicalPath)
-             new CarbonContext(sc, storePath, store.getCanonicalPath)
-           }
-           println("Carbon context available as cc.")
-           _cc
-         }
-              """)
-
-      processLine("import org.apache.spark.sql.SQLContext")
-      processLine("""
-         @transient val sqlContext = {
-           val _sqlContext = new SQLContext(sc)
-           println("SQL context available as sqlContext.")
-           _sqlContext
-         }
-              """)
-      processLine("import sqlContext.implicits._")
-      processLine("import sqlContext.sql")
-
-      processLine("import cc.implicits._")
-      processLine("import cc.sql")
+      processLine("import spark.implicits._")
+      processLine("import spark.sql")
       processLine("import org.apache.spark.sql.functions._")
+      processLine("import org.apache.spark.sql.CarbonSession")
+      processLine("import org.apache.spark.sql.SparkSession.Builder")
+      processLine(
+        """val metastoredb = this.getClass.getResource("../").getPath+"target/metastore/" """.stripMargin)
+      processLine("""println(":::::::::::::"+this.getClass.getResource("../").getPath+"target/metastore/")""")
+      processLine(
+        """val warehouse = this.getClass.getResource("../").getPath+"target/warehouse"""".stripMargin)
+      processLine("val _builder = new CarbonSession.CarbonBuilder(new Builder().config(\"spark" +
+                  ".sql.warehouse.dir\", s\"$warehouse\").config(\"javax.jdo.option" +
+                  ".ConnectionURL\",s\"jdbc:derby:;databaseName=$metastoredb;create=true\"));")
+      processLine("@transient val carbon = _builder.getOrCreateCarbonSession();")
+      processLine("println(\"Carbon session available as 'carbon'.\")")
+
     }
   }
 }
--- integration/spark2/src/main/scala/org/apache/spark/repl/carbon/Main.scala
+++ integration/spark2/src/main/scala/org/apache/spark/repl/carbon/Main.scala
@@ -17,31 +17,89 @@
 
 package org.apache.spark.repl.carbon
 
+import java.io.File
+
 import scala.tools.nsc.GenericRunnerSettings
 
-import org.apache.spark.repl.{CarbonSparkILoop, SparkILoop}
+import org.apache.spark.SparkConf
+import org.apache.spark.repl.Main._
+import org.apache.spark.repl.{CarbonSpark2ILoop, Signaling, SparkILoop}
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.internal.StaticSQLConf._
+import org.apache.spark.util.Utils
 
-object Main extends ILoop{
+object Main {
   private var _interp: SparkILoop = _
 
   def interp: SparkILoop = _interp
 
   private var hasErrors = false
 
+  val conf = new SparkConf()
+
+  val rootDir = conf.getOption("spark.repl.classdir").getOrElse(Utils.getLocalDir(conf))
+  val outputDir = Utils.createTempDir(root = rootDir, namePrefix = "repl")
+
   def interp_=(i: SparkILoop) { _interp = i }
 
   def main(args: Array[String]) {
-    _interp = new CarbonSparkILoop
-    val interpArguments = args.toList
+    _interp = new CarbonSpark2ILoop
+    val jars = Utils.getUserJars(conf, isShell = true).mkString(File.pathSeparator)
+    val interpArguments = List(
+      "-Yrepl-class-based",
+      "-Yrepl-outdir", s"${outputDir.getAbsolutePath}",
+      "-classpath", jars
+    ) ++ args.toList
 
     val settings = new GenericRunnerSettings(scalaOptionError)
     settings.processArguments(interpArguments, true)
-    _interp.process(settings)
+    if (!hasErrors) {
+      interp.process(settings)
+    }
   }
+    private def scalaOptionError(msg: String): Unit =
+    {
+      hasErrors = true
+      Console.err.println(msg)
+    }
 
-  private def scalaOptionError(msg: String): Unit = {
-    hasErrors = true
-    Console.err.println(msg)
+  def createSparkSession(): SparkSession = {
+    val execUri = System.getenv("SPARK_EXECUTOR_URI")
+    conf.setIfMissing("spark.app.name", "Spark shell")
+    // SparkContext will detect this configuration and register it with the RpcEnv's
+    // file server, setting spark.repl.class.uri to the actual URI for executors to
+    // use. This is sort of ugly but since executors are started as part of SparkContext
+    // initialization in certain cases, there's an initialization order issue that prevents
+    // this from being set after SparkContext is instantiated.
+    conf.set("spark.repl.class.outputDir", outputDir.getAbsolutePath())
+    if (execUri != null) {
+      conf.set("spark.executor.uri", execUri)
+    }
+    if (System.getenv("SPARK_HOME") != null) {
+      conf.setSparkHome(System.getenv("SPARK_HOME"))
+    }
+
+    val builder = SparkSession.builder.config(conf)
+    if (conf.get(CATALOG_IMPLEMENTATION.key, "hive").toLowerCase == "hive") {
+      if (SparkSession.hiveClassesArePresent) {
+        // In the case that the property is not set at all, builder's config
+        // does not have this value set to 'hive' yet. The original default
+        // behavior is that when there are hive classes, we use hive catalog.
+        sparkSession = builder.enableHiveSupport().getOrCreate()
+      } else {
+        // Need to change it back to 'in-memory' if no hive classes are found
+        // in the case that the property is set to hive in spark-defaults.conf
+        builder.config(CATALOG_IMPLEMENTATION.key, "in-memory")
+        sparkSession = builder.getOrCreate()
+      }
+    } else {
+      // In the case that the property is set but not to 'hive', the internal
+      // default is 'in-memory'. So the sparkSession will use in-memory catalog.
+      sparkSession = builder.getOrCreate()
+    }
+    sparkContext = sparkSession.sparkContext
+    Signaling.cancelOnInterrupt(sparkContext)
+    sparkSession
   }
 
 }
